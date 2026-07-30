ba83fb03aebc6c00434cb7bc26c75b63b7e9d802
Gu1lh3rme-rodrigues <grr2007guilherme@gmail.com>
2026-07-30 11:11:03 -0300

    Envio do dia 30-07

---
+++ Aulas/Dia 2/Exemplo_3.1.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+main(){
+	
+	int a, b;
+	
+	printf("Numeros de 1 a 50 em blocos de 10\n\n");
+	
+	for(a=1; a<=50; a++){
+		for (b=0; b<=; b++){
+			printf("Numero %i\n\n", a);
+		}
+	}
+}
---
+++ Aulas/Dia 2/Exemplo_3.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+//imprima os numeros de 1 a 50
+main(){
+	
+	int a;
+	
+	printf("Numeros de 1 a 50\n\n");
+	for (a=1; a<=50; a++){
+		printf("numero: %i\n", a);
+	}
+	printf("\n------------------\nFim do programa");	
+	
+}
